7aca738d8b3ebe238d6fe15d37fb50b0600c6119
danybr-dev <danybr92@gmail.com>
2016-01-17 13:49:57 +0100

    added vibration feedback

--- app/src/main/java/samurai/geeft/android/geeft/activity/LoginActivity.java
+++ app/src/main/java/samurai/geeft/android/geeft/activity/LoginActivity.java
@@ -1,8 +1,10 @@
 package samurai.geeft.android.geeft.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -50,7 +52,7 @@ public class LoginActivity extends AppCompatActivity implements TaskCallbackBool
     private CallbackManager callbackManager;
     private Animation mRotation;
     private long doneButtonClickTime;
-
+    private Vibrator vibe;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +60,9 @@ public class LoginActivity extends AppCompatActivity implements TaskCallbackBool
 
         //Default animation on social sign-in button press
         mRotation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.animation_rotate);
+
+        //Vibration feedback
+        vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         //On click the rotation animation starts.
         // mGoogleLoginButton.startAnimation(mRotation);
@@ -107,6 +112,7 @@ public class LoginActivity extends AppCompatActivity implements TaskCallbackBool
                 if (SystemClock.elapsedRealtime() - doneButtonClickTime < 1000) {
                     return;
                 }
+                vibe.vibrate(50); // Vibrate for 50 ms
                 //Store time of button click.
                 doneButtonClickTime = SystemClock.elapsedRealtime();
                 disableButtons();
@@ -168,6 +174,7 @@ public class LoginActivity extends AppCompatActivity implements TaskCallbackBool
                 if (SystemClock.elapsedRealtime() - doneButtonClickTime < 1000) {
                     return;
                 }
+                vibe.vibrate(50); // Vibrate for 50 ms
                 //Store time of button click.
                 doneButtonClickTime = SystemClock.elapsedRealtime();
                 //disableButtons();
