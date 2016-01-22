15a15b367d3bd9c90d5fa51157816eabaf5c830a
meltingpotteam <meltingpot.teamroma@gmail.com>
2016-01-22 18:55:40 +0100

    commiting to github for solving gradle problem

--- app/src/main/java/samurai/geeft/android/geeft/activity/MainActivity.java
+++ app/src/main/java/samurai/geeft/android/geeft/activity/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import samurai.geeft.android.geeft.R;
 
@@ -31,5 +33,24 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        /*if (id == R.id.action_logout) {
+            Toast.makeText(getApplicationContext(),"Logout",Toast.LENGTH_LONG);
+            return true;
+        }*/
+        return super.onOptionsItemSelected(item);
     }
 }
