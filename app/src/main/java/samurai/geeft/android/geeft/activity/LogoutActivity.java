527585fc34f6e9ba83b104ae247e38db46df9dd1
meltingpotteam <meltingpot.teamroma@gmail.com>
2016-01-25 14:34:07 +0100

    added translucent status bar

--- app/src/main/java/samurai/geeft/android/geeft/activity/LogoutActivity.java
+++ app/src/main/java/samurai/geeft/android/geeft/activity/LogoutActivity.java
@@ -3,11 +3,7 @@ package samurai.geeft.android.geeft.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.baasbox.android.BaasHandler;
 import com.baasbox.android.BaasResult;
@@ -27,15 +23,6 @@ public class LogoutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logout);
-
-        //setting colored translucent status bar
-        Window window = getWindow();
-        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            window.setStatusBarColor(ContextCompat.getColor(
-                    getApplicationContext(), R.color.colorPrimaryDark));
-        }
 
         BaasUser.current().logout(new BaasHandler<Void>() {
             @Override
