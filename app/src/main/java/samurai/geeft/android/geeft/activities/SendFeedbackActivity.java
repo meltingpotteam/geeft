94cf839be1e22face5244aafd6075382b81216e8
gabriel-dev <gabriele.vecchia91@gmail.com>
2016-02-15 11:27:10 +0100

    - fixed some strings in the send signalation page

--- app/src/main/java/samurai/geeft/android/geeft/activities/SendFeedbackActivity.java
+++ app/src/main/java/samurai/geeft/android/geeft/activities/SendFeedbackActivity.java
@@ -1,22 +1,12 @@
 package samurai.geeft.android.geeft.activities;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
-
 import samurai.geeft.android.geeft.R;
-import samurai.geeft.android.geeft.database.BaaSUploadGeeft;
-import samurai.geeft.android.geeft.fragments.AddGeeftFragment;
-import samurai.geeft.android.geeft.fragments.GeeftStoryListFragment;
-import samurai.geeft.android.geeft.interfaces.TaskCallbackBoolean;
-import samurai.geeft.android.geeft.models.Geeft;
 
 /**
  * Created by oldboy on 15/02/16.
@@ -30,7 +20,7 @@ public class SendFeedbackActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_feedback_help_page);
+        setContentView(R.layout.fragment_report_help_page);
 
         mToolbar = (Toolbar) findViewById(R.id.fragment_feedback_help_page_toolbar);
         setSupportActionBar(mToolbar);
