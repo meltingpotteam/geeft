5780a90ca3c4cd89fd3f39fe5dd37bbfc89b44d0
danybr-dev <danybr92@gmail.com>
2016-02-16 23:57:18 +0100

    GeeftItemAdapter with implementation of Signalisation button,BaaSSignalisationTask and TaskCallBackBooleanInt. If current user is "moderator" then he can delete directly the geeft,else if he is "registered" he can report through e-mail

---
+++ app/src/main/java/samurai/geeft/android/geeft/interfaces/TaskCallBackBooleanInt.java
@@ -0,0 +1,10 @@
+package samurai.geeft.android.geeft.interfaces;
+
+/**
+ * Created by danybr-dev on 16/02/2016.
+ */
+public interface TaskCallBackBooleanInt {
+
+    void done(boolean result,int action,String docId);
+
+}
